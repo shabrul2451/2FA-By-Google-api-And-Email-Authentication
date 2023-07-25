d3741b9f6988c84e0133d11915bbbdc8d68d0cfa
shabrul.islam <shabrul2451@gmail.com>
2023-07-25 14:33:46 +0600

    initial commit

--- go.mod
+++ go.mod
@@ -3,30 +3,13 @@ module gke-release-notes-api
 go 1.20
 
 require (
-	cloud.google.com/go/container v1.24.0
-	github.com/gorilla/mux v1.8.0
-	google.golang.org/api v0.132.0
-	google.golang.org/genproto v0.0.0-20230717213848-3f92550aa753
+	github.com/pquerna/otp v1.4.0
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
 require (
-	cloud.google.com/go/compute v1.20.1 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/s2a-go v0.1.4 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/oauth2 v0.10.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230706204954-ccb25ca9f130 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
-	google.golang.org/grpc v1.56.2 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- main.go
+++ main.go
@@ -1,66 +1,151 @@
 package main
 
 import (
-	container "cloud.google.com/go/container/apiv1"
-	"context"
-	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
-	"google.golang.org/api/option"
-	containerpb "google.golang.org/genproto/googleapis/container/v1"
-	"log"
-	"net/http"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
+	qrcode "github.com/skip2/go-qrcode"
+	"gopkg.in/gomail.v2"
 )
 
-type GKEReleaseNotes struct {
-	Version string `json:"version"`
+// Email configurations
+const (
+	smtpServer   = "YOUR_SMTP_SERVER"
+	smtpPort     = 587
+	smtpUsername = "YOUR_SMTP_USERNAME"
+	smtpPassword = "YOUR_SMTP_PASSWORD"
+)
+
+// generateCode generates a random 6-digit code.
+func generateCode() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%06d", rand.Intn(999999))
 }
 
-func GetGKEReleaseNotes(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	projectId := "default"
-	zone := "us-central1"
-	client, err := container.NewClusterManagerClient(ctx, option.WithUserAgent("123656749011-compute@developer.gserviceaccount.com"))
-	log.Println(err)
+// sendEmail sends the 2FA code to the user's email.
+func sendEmail(email, code string) error {
+	message := gomail.NewMessage()
+	message.SetHeader("From", smtpUsername)
+	message.SetHeader("To", email)
+	message.SetHeader("Subject", "Two-Factor Authentication Code")
+	message.SetBody("text/plain", "Your 2FA code is: "+code)
+
+	dialer := gomail.NewDialer(smtpServer, smtpPort, smtpUsername, smtpPassword)
+
+	if err := dialer.DialAndSend(message); err != nil {
+		return err
+	}
+	return nil
+}
+
+func TwoFAByMail() {
+	// For demonstration purposes, let's assume the user's email is known.
+	userEmail := "user@example.com"
+
+	// Generate the 2FA code
+	code := generateCode()
+
+	// Send the code via email
+	err := sendEmail(userEmail, code)
 	if err != nil {
-		http.Error(w, "Error creating GKE client", http.StatusInternalServerError)
+		fmt.Println("Error sending email:", err)
 		return
 	}
-	defer client.Close()
 
-	req := &containerpb.GetServerConfigRequest{
-		ProjectId: projectId, // Replace with your GCP project ID
-		Zone:      zone,      // Replace with your desired GCP zone
+	fmt.Println("2FA code sent to", userEmail)
+
+	// Simulate user input (replace this with your actual user input handling)
+	var userEnteredCode string
+	fmt.Print("Enter the 2FA code: ")
+	fmt.Scan(&userEnteredCode)
+
+	// Validate the entered code
+	if userEnteredCode == code {
+		fmt.Println("Authentication successful!")
+	} else {
+		fmt.Println("Authentication failed. Invalid code.")
 	}
+}
 
-	resp, err := client.GetServerConfig(ctx, req)
+//****************2FA By Google api*********************
+
+// generateTOTP generates a Time-based One-Time Password (TOTP).
+func generateTOTP(secret string) (string, error) {
+	key, err := otp.NewKeyFromURL(secret)
 	if err != nil {
-		http.Error(w, "Error getting GKE server config", http.StatusInternalServerError)
-		return
+		return "", err
 	}
 
-	var releaseNotes []GKEReleaseNotes
+	return totp.GenerateCode(key.Secret(), time.Now())
+}
 
-	for _, validVersion := range resp.GetValidMasterVersions() {
-		gkeReleaseNotes := GKEReleaseNotes{
-			Version: validVersion,
+func TwoFAByGoogleApi() {
+	// Replace "YourAppName" with the name of your application.
+	// This will be displayed in the user's 2FA app when they scan the QR code.
+	appName := "YourAppName"
+
+	// Generate a new TOTP secret for the user.
+	secret, _ := totp.Generate(totp.GenerateOpts{
+		Issuer:      appName,
+		AccountName: "user@example.com", // Replace with the user's email or username.
+	})
+
+	// Convert the TOTP secret to a URL that can be used to generate QR code.
+	qrCodeURL := secret.URL()
+
+	fmt.Println("Scan the following QR code using Google Authenticator or Authy:")
+	fmt.Println(qrCodeURL)
+
+	// Generate the QR code image and save it to a file.
+	qrCodeImg, err := qrcode.Encode(qrCodeURL, qrcode.Medium, 256)
+	if err != nil {
+		fmt.Println("Error generating QR code:", err)
+		os.Exit(1)
+	}
+
+	// Save the QR code image to a file (optional).
+	file, err := os.Create("qrcode.png")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	_, err = file.Write(qrCodeImg)
+	if err != nil {
+		fmt.Println("Error writing QR code to file:", err)
+		os.Exit(1)
+	}
+
+	// Wait for the user to scan the QR code and configure their 2FA app.
+	// Replace this with your actual user input handling.
+	fmt.Print("Press Enter when ready...")
+	fmt.Scanln()
+
+	// Verify the entered code.
+	if _, err := generateTOTP(secret.Secret()); err != nil {
+		fmt.Println("Error generating TOTP code:", err)
+		os.Exit(1)
+	} else {
+		// Simulate user input (replace this with your actual user input handling).
+		var userEnteredCode string
+		fmt.Print("Enter the 2FA code from your app: ")
+		fmt.Scan(&userEnteredCode)
+
+		// Verify the entered code.
+		if totp.Validate(userEnteredCode, secret.Secret()) {
+			fmt.Println("Authentication successful!")
+		} else {
+			fmt.Println("Authentication failed. Invalid code.")
 		}
-
-		releaseNotes = append(releaseNotes, gkeReleaseNotes)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(releaseNotes)
 }
 
 func main() {
-	r := mux.NewRouter()
-
-	// Define the API endpoint for getting the list of GKE release notes versions
-	r.HandleFunc("/api/gke/release-notes", GetGKEReleaseNotes).Methods("GET")
-
-	port := "8080"
-	fmt.Printf("Starting server on port %s...\n", port)
-	http.Handle("/", r)
-	http.ListenAndServe(":"+port, nil)
+	//TwoFAByMail()
+	TwoFAByGoogleApi()
 }
